5c0d364a2cb208ac89f392774c87c09f188b06f0
Hokusai97 <rw.rafalwiacek@gmail.com>
2024-05-17 19:57:57 +0200

    zad3

--- Main.java
+++ Main.java
@@ -2,33 +2,21 @@ import company.abstracts.Employee;
 import company.models.Manager;
 import company.models.Worker;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Employee[] employeesArray = new Employee[] {
-                new Worker("Worker 1", 1000, 1, "2024-05-17", "worker under 2"),
-                new Worker("Worker 2", 2000, 2, "2023-05-17", "worker under 3"),
-                new Worker("Worker 3", 3000, 3, "2022-05-17", "worker under 4"),
-                new Worker("Worker 4", 4000, 4, "2021-05-17", "worker under manager"),
-                new Manager("Manager 1", 20000, 5, "2020-05-17", "boss")
-        };
+        Worker worker1 = new Worker("Worker 1", 1000, 1, "2020-01-01", "worker under 2");
+        Worker worker2 = new Worker("Worker 2", 2000, 2, "2019-03-15", "worker under 3");
+        Worker worker3 = new Worker("Worker 3", 3000, 2, "2021-07-22", "worker under 4"); 
+        Manager manager1 = new Manager("Manager 1", 20000, 3, "2017-02-10", "boss");
 
-        ArrayList<Employee> employees = new ArrayList<>();
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(manager1.getName() + " has code: " + manager1.hashCode());
 
-        for (Employee employee : employeesArray) {
-            employees.add(employee);
-        }
-
-        for (Employee employee : employees) {
-            employee.work();
-            String employeeMessage = String.format("- %s (ID: %d, Position: %s, Hire date: %s, Salary: %.2f)",
-                    employee.getName(),
-                    employee.getId(),
-                    employee.getPosition(),
-                    employee.getHireDate(),
-                    employee.getSalary());
-            System.out.println(employeeMessage);
-        }
+        System.out.println("worker2.equals(Worker1): " + worker2.equals(worker1));
+        System.out.println("worker2.equals(Worker2): " + worker2.equals(worker2));
+        System.out.println("worker2.equals(Worker3): " + worker2.equals(worker3));
+        System.out.println("worker2.equals(Manager1): " + worker2.equals(manager1));
     }
 }
--- company/abstracts/Employee.java
+++ company/abstracts/Employee.java
@@ -6,15 +6,15 @@ public abstract class Employee implements Employable {
     private String name;
     private double salary;
     private int id;
-    private String hireDate; // Dodano pole hireDate
-    private String position; // Dodano pole position
+    private String hireDate;
+    private String position;
 
     public Employee(String name, double salary, int id, String hireDate, String position) {
         this.name = name;
         this.salary = salary;
         this.id = id;
-        this.hireDate = hireDate; // Inicjalizacja hireDate
-        this.position = position; // Inicjalizacja position
+        this.hireDate = hireDate;
+        this.position = position;
     }
 
     public String getName() {
@@ -29,17 +29,32 @@ public abstract class Employee implements Employable {
         return this.id;
     }
 
-    public String getHireDate() { // Dodano metodę getHireDate
+    public String getHireDate() {
         return this.hireDate;
     }
 
-    public String getPosition() { // Dodano metodę getPosition
+    public String getPosition() {
         return this.position;
     }
 
     @Override
-    public int hashCode() { // Dodano metodę hashCode
+    public int hashCode() {
         return this.id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (this == obj) {
+            return true;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee other = (Employee) obj;
+        return this.id == other.id;
     }
 
     public abstract void work();
